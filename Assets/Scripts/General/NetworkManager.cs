55f475c9852c280ed4f74b89fdbdf5fd55e9c015
dubnubdubnub <isaacchiu2006@gmail.com>
2022-04-13 21:02:40 -0600

    scruot setyop

---
+++ Assets/Scripts/General/NetworkManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UdpKit;
+using System;
+using Photon.Bolt;
+using Photon.Bolt.Matchmaking;
+
+public class NetworkManager : Photon.Bolt.GlobalEventListener
+{
+    [SerializeField]
+    private UnityEngine.UI.Text feedback;
+
+    public void FeedbackUser(string text)
+    {
+        feedback.text = text;
+    }
+
+    public void Connect()
+    {
+        FeedbackUser("Attempting to Connect....");
+        BoltLauncher.StartClient();
+    }
+
+    public override void SessionListUpdated(Map<Guid, UdpSession> sessionList)
+    {
+        FeedbackUser("Searching....");
+        BoltMatchmaking.JoinSession(HeadlessServerManager.RoomID());
+    }
+
+    public override void Connected(BoltConnection connection)
+    {
+        FeedbackUser("Connected!");
+    }
+}
