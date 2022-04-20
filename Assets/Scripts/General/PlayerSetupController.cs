9f4c472b267743f95b0dae9012089b276dadd09b
dubnubdubnub <isaacchiu2006@gmail.com>
2022-04-19 21:36:33 -0600

    BIG UPDATE

---
+++ Assets/Scripts/General/PlayerSetupController.cs
@@ -0,0 +1,37 @@
+﻿using Photon.Bolt;
+using UnityEngine;
+
+public class PlayerSetupController : GlobalEventListener
+{
+    [SerializeField]
+    private Camera _sceneCamera;
+
+    [SerializeField]
+    private GameObject _setupPanel;
+
+    public Camera SceneCamera { get => _sceneCamera; }
+
+    public override void SceneLoadLocalDone(string scene, IProtocolToken token)
+    {
+        if (!BoltNetwork.IsServer)
+        {
+            _setupPanel.SetActive(true);
+
+        }
+        Debug.Log("ran SceneLoadLocalDone");
+    }
+
+    public override void OnEvent(SpawnPlayerEvent evnt)
+    {
+        BoltEntity entity = BoltNetwork.Instantiate(BoltPrefabs.Player, new Vector3(0, 1, 0), Quaternion.identity);
+        entity.AssignControl(evnt.RaisedBy);
+        Debug.Log("ran onEvent");
+    }
+
+    public void SpawnPlayer()
+    {
+        SpawnPlayerEvent evnt = SpawnPlayerEvent.Create(GlobalTargets.OnlyServer);
+        evnt.Send();
+        Debug.Log("ran SpawnPlayer");
+    }
+}
---
+++ Assets/Scripts/Player/PlayerCallback.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using Photon.Bolt;
+
+public class NewBehaviourScript : EntityBehaviour<IPlayerState>
+{
+
+    private PlayerMotor _playerMotor;
+
+    private void Awake()
+    {
+        _playerMotor = GetComponent<PlayerMotor>();
+    }
+
+    public override void Attached()
+    {
+        state.AddCallback("LifePoints", UpdatePlayerLife);
+
+        if(entity.IsOwner)
+        {
+            state.LifePoints = _playerMotor.TotalLife;
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+            state.LifePoints += 10;
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+            state.LifePoints -= 10;
+    }
+
+    public void UpdatePlayerLife()
+    {
+        if(entity.HasControl)
+            GUI_Ctroller.Current.UpdateLife(state.LifePoints, _playerMotor.TotalLife);
+    }
+}
